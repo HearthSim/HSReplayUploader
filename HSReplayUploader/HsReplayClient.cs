06b05200131e147b49adfb70c9c48ce9f3230190
Alexander Zeier <alex@hearthsim.net>
2017-02-01 00:43:51 +0100

    v1.1.8

--- HSReplayUploader/HsReplayClient.cs
+++ HSReplayUploader/HsReplayClient.cs
@@ -18,7 +18,7 @@ namespace HSReplayUploader
 		public HsReplayClient(string apiKey, bool testData = false)
 		{
 			_testData = testData;
-			_client = new HSReplay.HsReplayClient(apiKey, "HSReplayUploader/1.1.7", testData);
+			_client = new HSReplay.HsReplayClient(apiKey, "HSReplayUploader/1.1.8", testData);
 		}
 
 		/// <summary>
